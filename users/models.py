4b37d7c06f4b97d01099ec8a49e71d591b9fa80c
Vadym Bihun <bihunva@gmail.com>
2023-05-01 22:50:06 +0300

    Create User model and custom manager for it

--- users/models.py
+++ users/models.py
@@ -1,3 +1,24 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+from users.managers import UserManager
+
+
+class User(AbstractUser):
+    email = models.EmailField(_("email address"), unique=True)
+    username = models.CharField(max_length=63, unique=True)
+    first_name = models.CharField(max_length=63,  blank=True)
+    last_name = models.CharField(max_length=63, blank=True)
+    bio = models.CharField(max_length=255,  blank=True)
+    avatar = models.ImageField(upload_to="avatars", blank=True)
+    subscribed_to = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="subscribers",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
