3b92f6b07c3f99e84b198e0b59d4e2f59aa903c8
Vadym Bihun <bihunva@gmail.com>
2023-05-02 00:07:33 +0300

    Start posts app & add it to INSTALLED_APPS

---
+++ posts/models.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="posts"
+    )
+
+    def __str__(self) -> str:
+        return self.title
