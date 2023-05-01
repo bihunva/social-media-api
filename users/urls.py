2f2533e7ac2986b7b34ad8c246f3a43941f0eb7d
Vadym Bihun <bihunva@gmail.com>
2023-05-01 22:52:15 +0300

    Include users end-points into main urls.py

---
+++ users/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from users.views import CreateUserView, ManageUserView, UserViewSet
+
+router = routers.DefaultRouter()
+router.register("", UserViewSet)
+
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("me/", ManageUserView.as_view(), name="manage"),
+    path("",  include(router.urls)),
+]
+
+app_name = "users"
